4aa247ad5e7d1995dde8ed9408e6ad5265dc5e22
minamibashi rearn <rearn499+git@gmail.com>
2017-10-03 23:18:16 +0900

    エラーとなるテストコードの追加

--- tests/test_des.py
+++ tests/test_des.py
@@ -19,6 +19,16 @@ class test_des(unittest.TestCase):
         self.assertTrue(self.is_under_8_octets(0x1145148108931919))
         self.assertFalse(self.is_under_8_octets(0x1234567890abcdef0))
 
+    def test_encode_raieses(self):
+        assertRaises(self.des.encode(0xfedcba9876543210f))
+
+    def test_decode_raieses(self):
+        assertRaises(self.des.decode(0x12345678fedcba098))
+
+    def test_des_init_raieses(self):
+        assertRaises(des.des(b'0123456789'))
+        assertRaises(des.des(b'98765'))
+
 
 if __name__ == '__main__':
     unittest.main()
