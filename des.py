c1144e340c1dc3695aa4eb4d116147100efed7e1
minamibashi rearn <rearn499+git@gmail.com>
2017-10-08 10:47:01 +0900

    is_under_8_octets を local 関数に変更

--- des.py
+++ des.py
@@ -9,17 +9,17 @@ class des():
             raise Exception()
 
     def encode(self, plain_number):
-        if(self.is_under_8_octets(plain_number)):
+        if(self.__is_under_8_octets(plain_number)):
             return self.des.encrypt(plain_number.to_bytes(8, 'big'))
         else:
             raise Exception()
 
     def decode(self, cipher_number):
-        if(self.is_under_8_octets(int.from_bytes(cipher_number, 'big'))):
+        if(self.__is_under_8_octets(int.from_bytes(cipher_number, 'big'))):
             ret = self.des.decrypt(cipher_number)
             return int.from_bytes(ret, 'big')
         else:
             raise Exception()
 
-    def is_under_8_octets(self, number):
+    def __is_under_8_octets(self, number):
         return number == (number & 0xffffffffffffffff)
