4e12650fd10b63ba7d5daa7e07cf4e3e25c7d2ce
minamibashi rearn <rearn499+git@gmail.com>
2017-10-09 03:25:24 +0900

    テストコード間違えてた

--- tests/test_dbi.py
+++ tests/test_dbi.py
@@ -14,19 +14,19 @@ class test_dbi(unittest.TestCase):
         self.db = dbi.dbi('mongodb://localhost/testdb', 'testdb')
 
     def test_find_uri(self):
-        self.assertTrue(self.db.find_uri(0), 'http://example.com/')
-        self.assertTrue(self.db.find_uri(1), 'http://example.org/test.html')
+        self.assertEqual(self.db.find_uri(0), 'http://example.com/')
+        self.assertEqual(self.db.find_uri(1), 'http://example.org/test.html')
         self.assertIsNone(self.db.find_uri(42))
 
     def test_find_id(self):
-        self.assertTrue(self.db.find_id('http://example.com/'), 0)
-        self.assertTrue(self.db.find_id('http://example.org/test.html'), 1)
+        self.assertEqual(self.db.find_id('http://example.com/'), 0)
+        self.assertEqual(self.db.find_id('http://example.org/test.html'), 1)
         self.assertIsNone(self.db.find_id('http://example.org/none.html'))
 
     def test_update(self):
-        self.assertTrue(self.db.update('http://example.com/'), 0)
-        self.assertTrue(self.db.update('http://example.org/test.html'), 1)
+        self.assertEqual(self.db.update('http://example.com/'), 0)
+        self.assertEqual(self.db.update('http://example.org/test.html'), 1)
         ret = self.db.find_id('http://example.net/')
-        self.assertTrue(self.db.find_uri(ret), 'http://example.net/')
-        self.assertTrue(self.db.find_id('http://example.net/'), ret)
-        self.assertTrue(self.db.update('http://example.net/'), ret)
+        self.assertEqual(self.db.find_uri(ret), 'http://example.net/')
+        self.assertEqual(self.db.find_id('http://example.net/'), ret)
+        self.assertEqual(self.db.update('http://example.net/'), ret)
