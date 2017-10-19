c7bdee1424badfbf51c9a31a36cedba19f5cca7a
minamibashi rearn <rearn499+git@gmail.com>
2017-10-20 00:05:06 +0900

    とりあえず読み込めるようにする

---
+++ main.py
@@ -0,0 +1,34 @@
+import base64_32
+import dbi
+import des
+from flask import abort, Flask, redirect
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    pass
+
+
+@app.route('/<string:base>')
+def path(base):
+    if len(base) == 11:
+        code = base64_32.base64decode(base)
+    elif len() == 13:
+        code = base64_32.base32decode(base)
+    else:
+        return abort(404)
+    num = crypt.decode(code)
+    uri = db.find_uri(num)
+    app.logger.debug({'base': base, 'code': code, 'num': num, 'uri': uri})
+    if uri is None:
+        return abort(404)
+    else:
+        return redirect(uri, code=301)
+
+
+if __name__ == '__main__':
+    crypt = des.des('12345678')
+    db = dbi.dbi('mongodb://localhost/flaskdb', 'flaskdb')
