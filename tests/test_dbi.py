f7d6ec640de303e23be7b0d76e174967c12279d7
minamibashi rearn <rearn499+git@gmail.com>
2017-10-24 23:16:16 +0900

    文字列に変更しても問題ないかを検証

--- tests/test_dbi.py
+++ tests/test_dbi.py
@@ -11,7 +11,7 @@ class test_dbi(unittest.TestCase):
             db_uri.insert_one({'id': 0, 'uri': 'http://example.com/', 'type': 0})
             db_uri.insert_one({'id': 1, 'uri': 'http://example.org/test.html', 'type': 1})
             db_cs = db.counters
-            db_cs.insert_one({'id': 'uri_id', 'seq': 2})
+            db_cs.insert_one({'id': 'uri_id', 'seq': '2'})
         self.db = dbi.dbi('mongodb://travis:test@localhost/testdb', 'testdb')
 
     def tearDown(self):
