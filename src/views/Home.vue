418b2a7835b8066aedee16462f5b1a65270af718
minamibashi rearn <minamibashi@rearn.info>
2019-04-03 00:43:40 +0900

    ライブラリなどを併せる

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  div#app
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(v-bind:msg="message")
 </template>
+
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
