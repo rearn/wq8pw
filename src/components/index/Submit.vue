7ad924fe77aeda373a954c5d09fbc9ca66ea6707
minamibashi rearn <minamibashi@rearn.info>
2019-04-11 23:45:17 +0900

    テンプレートの分割終了

---
+++ src/components/index/Submit.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  section#submit
+    p
+      | 以下でURLが生成できます。
+    p
+      | ※サイト閉鎖時にすべてのURL対応表を公開しますので、機密情報は入れないでください。
+    form(method="post" action="/accept/post" v-on:submit.prevent="warn('aaa', $event)")
+        p
+          label
+            | URL: 
+            input(type="url" name="uri" v-model="uri" required)
+        p
+          label
+            input(type="checkbox" name="jamp_flag" v-model="jamp_flag")
+            | 自動ジャンプせず警告ページを表示する。
+        // div(id="g-recaptcha" data-sitekey="6LfEWDYUAAAAAPUPSx6IAIz1NUCCtRHfnEUp8xSY") 
+        button(type="submit" value="submit") 送信する
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Submit extends Vue {
+  @Prop() private msg!: string;
+  uri = '';
+  jamp_flag = false;
+  warn(msg: string, event: Event) {
+    const uri: string = this.uri;
+    const jamp_flag = this.jamp_flag;
+    alert([msg, uri, jamp_flag].toString());
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,26 @@
 <template lang="pug">
   div#app
-    Header(v-bind:msg="message" )
-    section#main
-      h2 当サイトについて
-      p
-        |  当サイトは短縮URLサービスです。
-        |  なにかありましたら、 
-        span Twitter: 
-          a(href="https://twitter.com/rearn499") @rearn499
-        | に連絡お願いします。
-    section#submit
-      p
-        | 以下でURLが生成できます。
-      p
-        | ※サイト閉鎖時にすべてのURL対応表を公開しますので、機密情報は入れないでください。
-      form(method="post" action="/accept/post" v-on:submit.prevent="warn('aaa', $event)")
-          p
-            label
-              | URL: 
-              input(type="url" name="uri" v-model="uri" required)
-          p
-            label
-              input(type="checkbox" name="jamp_flag" v-model="jamp_flag")
-              | 自動ジャンプせず警告ページを表示する。
-          // div(id="g-recaptcha" data-sitekey="6LfEWDYUAAAAAPUPSx6IAIz1NUCCtRHfnEUp8xSY") 
-          button(type="submit" value="submit") 送信する
+    Header(v-bind:msg="message")
+    Main()
+    Submit()
+    
 </template>
 
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Header from '@/components/index/Header.vue'; // @ is an alias to /src
+import Main from '@/components/index/Main.vue';
+import Submit from '@/components/index/Submit.vue';
 
 @Component({
   components: {
     Header,
+    Main,
+    Submit,
   },
 })
 export default class Home extends Vue {
   message = '短縮URLサービス wq8pw にようこそ';
-  uri = '';
-  jamp_flag = false;
-  warn(msg: string, event: Event) {
-    const uri: string = this.uri;
-    const jamp_flag = this.jamp_flag;
-    alert([msg, uri, jamp_flag].toString());
-  }
 }
 </script>
