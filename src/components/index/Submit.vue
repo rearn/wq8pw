a957243044b99bc57eab1f403806c668c7c09734
minamibashi rearn <minamibashi@rearn.info>
2019-04-14 08:02:21 +0900

    ちょっとだけデザインをイジる

--- src/components/index/Submit.vue
+++ src/components/index/Submit.vue
@@ -32,18 +32,9 @@ export default class Submit extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+form {
+  width: 25em;
+  border: 1px solid #aaa;
+  margin: auto;
 }
 </style>
