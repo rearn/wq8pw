99719c6a2cbf447762ea86624b274d6384482aaf
minamibashi rearn <minamibashi@rearn.info>
2019-04-14 11:36:58 +0900

    url のチェックを追加

--- srv/routes/api/v1/index.ts
+++ srv/routes/api/v1/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { Des, UriString } from '../../../modules/des';
 import { Db, redirectType } from '../../../modules/dbi';
 import * as store from '../../../modules/store';
+import * as adjuster from 'adjuster';
 const router = express.Router();
 
 const c: Des = store.c;
@@ -10,6 +11,9 @@ const db: Db = store.db;
 
 router.post('/accept/post', async (req, res) => {
   const uri: string = req.body.uri;
+  if (! adjuster.STRING.PATTERN.URI.test(uri)) {
+    res.status(400).end();
+  }
   const type: redirectType = ('jamp_flag' in req.body) ? redirectType.antenna : redirectType.redirect;
 
   const id: Uint32Array = await db.update(uri, type);
