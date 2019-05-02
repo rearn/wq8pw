e08e99959f21b472d8859a2afcec274f9cb00989
minamibashi rearn <minamibashi@rearn.info>
2019-05-03 02:51:00 +0900

    mongoose を閉じる

--- tests/unit/express/post.spec.ts
+++ tests/unit/express/post.spec.ts
@@ -4,9 +4,14 @@
 
 import request from 'supertest';
 import app from '../../../srv/test';
+import mongoose from 'mongoose';
 
 describe('POST', () => {
   it('400', () => {
     return request(app).post('/api/v1/accept/post').expect(400);
   });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
 });
