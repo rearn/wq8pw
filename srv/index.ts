87ce17a1cc889a71124aff8cf6d1992837f2440c
minamibashi rearn <minamibashi@rearn.info>
2020-07-28 21:41:08 +0900

    移動ミスの修正

--- srv/index.ts
+++ srv/index.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import main from './routes/';
 import apiV1 from './routes/api/v1/';
 import master from './routes/api/master/v1/';
-import { httpLogger } from './logger';
+import { httpLogger } from './modules/logger';
 
 export default (app: any, http: any) => {
   app.use(express.json());
