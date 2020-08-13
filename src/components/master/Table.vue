3f23b0eab5a17df21968fae75632ed5c3c4a1fe2
minamibashi rearn <minamibashi@rearn.info>
2020-08-13 12:24:54 +0900

    id を表示させる

--- src/components/master/Table.vue
+++ src/components/master/Table.vue
@@ -2,11 +2,13 @@
   table
     tbody
       tr
+        th id
         th short
         th long
         th uri
         th type
       tr(v-for="item in message")
+        td {{ item.id }}
         td {{ item.encrypt.short }}
         td {{ item.encrypt.long }}
         td {{ item.uri }}
